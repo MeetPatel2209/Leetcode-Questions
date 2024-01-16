ddac05a40f83e20da842a6d4edd9f9464999884a
Meet Patel <101829882+MeetPatel2209@users.noreply.github.com>
2024-01-16 09:40:41 +0530

    Time: 169 ms (72.51%), Space: 97.4 MB (45.71%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -1,46 +1,48 @@
 class RandomizedSet {
+    vector<int> v;
+    unordered_map<int,int> mp;
 public:
-    unordered_set<int> ele;
-    int size;
+   
     RandomizedSet() {
-        size=0;
     }
+
+    bool search(int val){
+
+         if(mp.find(val)!=mp.end())
+            return true;
+         return false;
+
+    }
+
     
     bool insert(int val) {
-        if(ele.find(val) == ele.end()) {
-            ele.insert(val);
-            return true;
-        }
-        else {
+
+        if(search(val))
             return false;
-        }
+
+        v.push_back(val);
+        mp[val] = v.size()-1;
+        return true;
     }
+
     
     bool remove(int val) {
-        if(ele.find(val) != ele.end()) {
-            ele.erase(val);
-            return true;
-        }
-        else {
+
+        if(!search(val))
             return false;
-        }
+
+       
+        auto it = mp.find(val);
+        v[it->second] = v.back();
+        v.pop_back();
+        mp[v[it->second]] = it->second;
+        mp.erase(val);
+        return true;
     }
-    
+
+   
     int getRandom() {
-        int n = ele.size();
-        int ind = (rand()%n);
-        auto it = ele.begin();
-        while(ind--) {
-            it++;
-        }
-        return *it;
+
+        return v[rand()%v.size()];
     }
 };
-
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet* obj = new RandomizedSet();
- * bool param_1 = obj->insert(val);
- * bool param_2 = obj->remove(val);
- * int param_3 = obj->getRandom();
- */
